6ac499b8e3b606a022ef3432c4614449022fb5a1
Davide Casiraghi <davide.casiraghi@gmail.com>
2021-07-09 14:15:36 +0200

    Add Company and Employee models

---
+++ app/Models/Employee.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Employee extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'company_id',
+        'first_name',
+        'last_name',
+        'email',
+        'phone',
+    ];
+
+    /**
+     * Return the company of the employee
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
+
+}
